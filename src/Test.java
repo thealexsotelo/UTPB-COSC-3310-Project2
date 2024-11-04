d47695813c44adc86c23cd428100935cd3fc9982
Tim <fielder_t@utpb.edu>
2024-11-04 11:03:41 -0600

    Cleanup of unused files.

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-import javafx.scene.text.Font;
-
-public class Test {
-    public static void main(String[] args) {
-        for (String s : Font.getFontNames()) {
-            System.out.println(s);
-        }
-    }
-}
